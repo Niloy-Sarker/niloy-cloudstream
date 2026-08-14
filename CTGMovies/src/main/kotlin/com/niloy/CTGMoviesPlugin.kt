8ba32f0a9a130c0add0d39eb5e569d69903fd2b9
Niloy Sarker <nsniloy21@gmail.com>
2026-08-15 00:05:18 +0600

    feat: implement CTGMoviesPlugin and initialize build artifacts

---
+++ CTGMovies/src/main/kotlin/com/niloy/CTGMoviesPlugin.kt
@@ -0,0 +1,12 @@
+package com.niloy
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class CTGMoviesPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(CTGMoviesProvider())
+    }
+}
